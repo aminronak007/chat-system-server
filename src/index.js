1250a55b633895b10388d442a60c56675f9487ed
ronak amin <aminronak007@gmail.com>
2023-07-26 12:37:25 +0530

    Cors error solution_2

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const PORT = vars.port;
 
 app.use(expressValidator());
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:3000",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
