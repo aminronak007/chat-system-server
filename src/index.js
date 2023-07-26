48268ecdbcfe7e7bea775ed6dc5f2607a9f17ff0
ronak amin <aminronak007@gmail.com>
2023-07-26 12:31:57 +0530

    Cors error solution1

--- src/index.js
+++ src/index.js
@@ -18,14 +18,14 @@ const app = express();
 const PORT = vars.port;
 
 app.use(expressValidator());
-// const corsOptions = {
-//   origin: "http://localhost:3000",
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(mongoSanitize());
 app.use(express.static("uploads"));
 
