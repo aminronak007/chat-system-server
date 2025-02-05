dc20a1c9bb852662342c2566b48cd7fdb0ba4368
Uss Dev 1 <ussdev1@unitesoftwaresolutions.com>
2025-02-05 09:44:13 +0530

    Add Netlify URL for CORS errors

--- src/index.js
+++ src/index.js
@@ -19,7 +19,11 @@ const PORT = vars.port;
 
 app.use(expressValidator());
 const corsOptions = {
-  origin: ["http://localhost:3000", "http://localhost:3001"],
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://ar-chat-app.netlify.app",
+  ],
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
